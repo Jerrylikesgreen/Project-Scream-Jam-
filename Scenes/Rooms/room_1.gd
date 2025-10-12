e3674968483ffbbd508c6f2eaf226aeab4440146
Jerrylikesgreen <gerardo.azamar11@gmail.com>
2025-10-11 22:09:45 -0400

    Refactor killer spawn logic and add debug label
    Refactored killer_manager.gd to simplify killer tracking and removed the enemy queue system. Updated room_1.gd to spawn the killer with a countdown if killer_in_other_room is set, and display the countdown using a new Debug label added to room_1.tscn. Adjusted test_level.tscn spawn point position and added a temporary scene file.

--- Scenes/Rooms/room_1.gd
+++ Scenes/Rooms/room_1.gd
@@ -1,12 +1,31 @@
 class_name Room1 extends Node2D
 
+@onready var debug: Label = %Debug
 
 @onready var spawn_point: Node2D = %SpawnPoint
+var killer_spawn_countdown: Timer
+
 
 func _ready() -> void:
 	if get_tree().get_nodes_in_group("Player").is_empty():
 		var player = Globals.PLAYER.instantiate()
 		add_child(player)
 		player.global_position = spawn_point.global_position
-	if get_tree().get_nodes_in_group("Killer").is_empty():
-		KillerManager.spawn_enemies_for_scene(self)
+	if KillerManager.killer_in_other_room:
+		killer_spawn_countdown = Timer.new()
+		add_child(killer_spawn_countdown)
+		killer_spawn_countdown.set_wait_time(10.0)
+		killer_spawn_countdown.timeout.connect(_on_killer_countdown_timeout)
+		killer_spawn_countdown.start()
+
+func _process(delta: float) -> void:
+	if killer_spawn_countdown:
+		var wt: String =   str(killer_spawn_countdown.get_time_left()) 
+		debug.set_text(wt)
+	
+
+func _on_killer_countdown_timeout() ->void:
+	var killer_instance =  KillerManager.KILLER.instantiate()
+	add_child(killer_instance)
+	killer_instance.position = spawn_point.global_position
+	KillerManager.killer_in_other_room = false
