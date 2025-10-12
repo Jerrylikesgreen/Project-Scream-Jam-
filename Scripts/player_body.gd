c46b0d8e16675ca1d408cc1c0b4d3e1a9b2f2980
Jerrylikesgreen <gerardo.azamar11@gmail.com>
2025-10-12 00:55:50 -0400

    Add interactible object system and update player
    Introduced a new InteractibleObject scene and script, and integrated an ActionArea to the player for interaction detection. PlayerBody logic was moved to a new script, and test_level now includes the interactible object for testing. These changes lay the groundwork for interactible gameplay elements.

---
+++ Scripts/player_body.gd
@@ -0,0 +1,13 @@
+class_name PlayerBody extends CharacterBody2D
+
+@onready var action_area: Area2D = %ActionArea
+@onready var player_controller: PlayerController = %PlayerController
+
+func _ready() -> void:
+	Events.player_hit_signal.connect(_on_player_hit)
+	
+	
+func _on_player_hit():
+	get_parent().queue_free()
+	
+	pass
