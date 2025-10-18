f7c2716818e4878b9638235dcd11bf4a3498a118
February71st <lbowen@g.hmc.edu>
2025-10-18 01:52:46 -0700

    Made killer slide on walls, changed hitbox,fixed error from changed hitbox

--- Autoloads/globals.gd
+++ Autoloads/globals.gd
@@ -2,7 +2,7 @@
 extends Node
 const PLAYER = preload("uid://bja82w2ernttj")
 
-var player = null
+var player:Player = null
 const ROOM_1_FRONT_ENTRENCE_OUTSIDE = preload("uid://cvl8kf1nyvo88")
 
 const DATA = preload("uid://bh74vn115iw2k")
